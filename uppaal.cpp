2ebb044de52e7c9327581ac991a1376888d1b925
LowkeyCoding <loke0046@gmail.com>
2025-10-07 13:16:46 +0200

    Added UPPAAL wrapper for creating a density matrix from a binary string

--- uppaal.cpp
+++ uppaal.cpp
@@ -4,7 +4,13 @@ extern "C" { // tells C++ compiler to use C symbol name mangling (C compiler ign
 #endif // __cplusplus
 
 
-void UApplyGate(double* rho, int rho_size, int32_t gate, int32_t target) {
+void UInitBinState(double* rho, int rho_size, const char* state) {
+    assert(size_t(ceil(rho_size/2)) == strlen(state));
+    cx_mat res = BinaryStringToDensityMatrix(state);
+    FromMatrix(res, rho, rho_size);
+}
+
+void UApplyGate(double* rho, int rho_size, int gate, int target) {
     cx_mat res = ApplyGate(ToMatrix(rho,rho_size), static_cast<u_gate>(gate), target);
     FromMatrix(res, rho, rho_size);
 }
@@ -14,16 +20,16 @@ void UAmplitudeDampeningAndDephasing(double* rho, int rho_size, double* T1, doub
     FromMatrix(res, rho, rho_size);
 }
 
-void UMeasureAll(double* rho, int rho_size, double* random_values, int32_t* res, int32_t smaple_count) {
+void UMeasureAll(double* rho, int rho_size, double* random_values, int* res, int smaple_count) {
     vector<double> rv = vector<double>(random_values, (random_values + sizeof(double)*smaple_count));    
-    vector<int32_t> vres = Sample(ToMatrix(rho,rho_size), rv);
+    vector<int> vres = Sample(ToMatrix(rho,rho_size), rv);
     for(size_t i = 0; i < vres.size(); i++) {
         res[i] = vres[i];
     }
 }
-cx_mat ToMatrix(double* matrix, int32_t size){
+cx_mat ToMatrix(double* matrix, int size){
     cx_double test[size*size];
-    for (int32_t i = 0; i < size*size; i++)
+    for (int i = 0; i < size*size; i++)
     {
         test[i] = cx_double(matrix[i*2], matrix[i*2+1]); 
     }
--- uppaal.h
+++ uppaal.h
@@ -2,10 +2,11 @@
 #ifdef __cplusplus
 extern "C" { // tells C++ compiler to use C symbol name mangling (C compiler ignores)
 #endif // __cplusplus
-void UMeasureAll(double* rho, int rho_size, double* random_values, int32_t* res, int32_t smaple_count);
-void UApplyGate(double* rho, int rho_size, int32_t gate, int32_t target);
+void UInitBinState(double* rho, int rho_size, const char* state);
+void UMeasureAll(double* rho, int rho_size, double* random_values, int* res, int smaple_count);
+void UApplyGate(double* rho, int rho_size, int gate, int target);
 void UAmplitudeDampeningAndDephasing(double* rho, int rho_size, double* T1, double* T2, double t);
-cx_mat ToMatrix(double* matrix, int32_t size);
+cx_mat ToMatrix(double* matrix, int size);
 void FromMatrix(cx_mat matrix, double* ret, int size);
 #ifdef __cplusplus
 } // end of "C" symbol name mangling
