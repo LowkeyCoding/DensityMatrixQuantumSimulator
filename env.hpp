ce5ec3f85e307ea5be516d8387314cb8e6e4b631
LowkeyCoding <loke0046@gmail.com>
2025-10-29 11:07:25 +0100

    Removed unused files

--- env.hpp
+++
@@ -1,9 +0,0 @@
-#include <unordered_map>
-#include <armadillo>
-#include <cassert>
-#ifndef ENV_H
-#define ENV_H
-using arma::mat;
-using arma::cx_double;
-using arma::cx_mat;
-#endif
--- examples/densecoding.cpp
+++ examples/densecoding.cpp
@@ -2,23 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-    double test [32] = {0};
-    double T1[2] = {20,20};
-    double T2[2] = {18,18};
-    for(int i = 0; i < 1000000; i++) {
-        UInitBinState(test, 2, "00");
-        UApplyGate(test, 2, GH, 0);
-        UAmplitudeDampeningAndDephasing(test, 2, T1, T2, 1);
-        UApplyCGate(test, 2, GX, 0, 1);
-        UAmplitudeDampeningAndDephasing(test, 2, T1, T2, 1);
-        UApplyGate(test, 2, GX,0);
-        UAmplitudeDampeningAndDephasing(test, 2, T1, T2, 1);
-        UApplyGate(test, 2, GZ,0);
-        UAmplitudeDampeningAndDephasing(test, 2, T1, T2, 1);
-        UApplyCGate(test, 2, GX, 0, 1);
-        UAmplitudeDampeningAndDephasing(test, 2, T1, T2, 1);
-        UApplyGate(test, 2, GH, 0);
-        UAmplitudeDampeningAndDephasing(test, 2, T1, T2, 1);
-        UMeasureAll(test, 2, (double)rand() / (double)RAND_MAX);
+    const int qc = 5;
+    double test [2048] = {0};
+    double T1[qc] = {20};
+    double T2[qc] = {18};
+    for(int i = 0; i < 10000; i++) {
+        UInitBinState(test, qc, "00000");
+        UApplyGate(test, qc, GH, 0);
+        UAmplitudeDampeningAndDephasing(test, qc, T1, T2, 1);
+        UApplyCGate(test, qc, GX, 0, 1);
+        UAmplitudeDampeningAndDephasing(test, qc, T1, T2, 1);
+        UApplyGate(test, qc, GX,0);
+        UAmplitudeDampeningAndDephasing(test, qc, T1, T2, 1);
+        UApplyGate(test, qc, GZ,0);
+        UAmplitudeDampeningAndDephasing(test, qc, T1, T2, 1);
+        UApplyCGate(test, qc, GX, 0, 1);
+        UAmplitudeDampeningAndDephasing(test, qc, T1, T2, 1);
+        UApplyGate(test, qc, GH, 0);
+        UAmplitudeDampeningAndDephasing(test, qc, T1, T2, 1);
+        UMeasureAll(test, qc, (double)rand() / (double)RAND_MAX);
     }
 }
